5d0247f881b637faabb58c7509816fe0351bb951
Arjun S <arjun.mi1@iiitmk.ac.in>
2017-05-02 23:23:31 +0530

    Button executing Commands su Access granted TODO execute su commands

---
+++ SOOkayGoogle/app/src/main/java/com/omegadevs/sookaygoogle/ShellExecuter.java
@@ -0,0 +1,30 @@
+package com.omegadevs.sookaygoogle;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by arjuns on 02/05/17.
+ */
+
+public class ShellExecuter {
+    public ShellExecuter(){
+
+    }
+    public String Executer(String command){
+        StringBuffer output = new StringBuffer();
+        Process p;
+        try{
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = "";
+            while((line = reader.readLine())!= null){
+                output.append(line + "\n");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        String response = output.toString();
+        return response;
+    }
+}
