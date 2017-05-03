07a75012118eebdc4348c3fe885f7ebcdf609cfd
Arjun S <arjun.mi1@iiitmk.ac.in>
2017-05-03 14:13:06 +0530

    Functinal buttons with On and Off battery charging mimic functional

--- SOOkayGoogle/app/src/main/java/com/omegadevs/sookaygoogle/MainActivity.java
+++ SOOkayGoogle/app/src/main/java/com/omegadevs/sookaygoogle/MainActivity.java
@@ -1,42 +1,63 @@
 package com.omegadevs.sookaygoogle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.app.Activity;
-import com.omegadevs.sookaygoogle.ShellExecuter;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnOn,btnOff;
-    String command;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnOn = (Button)findViewById(R.id.on);
-//        btnOff = (Button)findViewById(R.id.off);
-        final ShellExecuter exe = new ShellExecuter();
+        btnOff = (Button) findViewById(R.id.off);
+        try {
+            Process su = Runtime.getRuntime().exec("su");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         btnOn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                command = "su";
-                exe.Executer(command);
-                command = "dumpsys battery set ac 1";
-                exe.Executer(command);
-                command = "";
+                Toast.makeText(getApplicationContext(), "Executed", Toast.LENGTH_SHORT).show();
+                try {
+                    Process su = Runtime.getRuntime().exec("su");
+                    DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
+                    outputStream.writeBytes("dumpsys battery set ac 1\n");
+                    outputStream.flush();
+                    outputStream.writeBytes("exit\n");
+                    outputStream.flush();
+                    su.waitFor();
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
-//        btnOff.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                command = "dumpsys battery reset";
-//                exe.Executer(command);
-//                command = "";
-//            }
-//        });
+        btnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Executed", Toast.LENGTH_SHORT).show();
+                try {
+                    Process su = Runtime.getRuntime().exec("su");
+                    DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
+                    outputStream.writeBytes("dumpsys battery reset\n");
+                    outputStream.flush();
+                    outputStream.writeBytes("exit\n");
+                    outputStream.flush();
+                    su.waitFor();
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
